02f5122a3a82cd0c59d1c4e8be275766b866a943
Ntungufhadzeni <mbudzenin@thenakedscientists.co.za>
2023-08-18 22:59:22 +0200

    fix debug env

--- thuso/settings.py
+++ thuso/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv('DEBUG'))
+DEBUG = bool(os.environ.get('DEBUG'))
 
 HOST = os.getenv('HOST')
 
