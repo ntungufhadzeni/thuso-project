c8bc46537adbfee7a0d353751cfe0edc5863e7c8
Ntungufhadzeni <mbudzenin@thenakedscientists.co.za>
2023-08-21 17:54:24 +0200

    optimize

--- cover_letter/models.py
+++ cover_letter/models.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 
 class TimeStampedModel(models.Model):
     id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, max_length=34)
+    date_created = models.DateTimeField(default=timezone.now)
 
     class Meta:
         abstract = True
@@ -14,7 +15,6 @@ class TimeStampedModel(models.Model):
 class CoverLetterSubscriber(TimeStampedModel):
     whatsapp_name = models.CharField(max_length=100)
     whatsapp_number = models.CharField(max_length=11, unique=True)
-    date_created = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f'{self.whatsapp_name}'
