eca389f5811dd1ced531beb4017301da9bbd61bd
Ntungufhadzeni <mbudzenin@thenakedscientists.co.za>
2023-08-21 22:08:32 +0200

    add ordering

--- cover_letter/models.py
+++ cover_letter/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 
 class TimeStampedModel(models.Model):
     id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, max_length=34)
-    date_created = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(default=timezone.now)
 
     class Meta:
         abstract = True
@@ -15,6 +15,9 @@ class TimeStampedModel(models.Model):
 class CoverLetterSubscriber(TimeStampedModel):
     whatsapp_name = models.CharField(max_length=100)
     whatsapp_number = models.CharField(max_length=11, unique=True)
+
+    class Meta:
+        ordering = ["-created_at"]
 
     def __str__(self):
         return f'{self.whatsapp_name}'
