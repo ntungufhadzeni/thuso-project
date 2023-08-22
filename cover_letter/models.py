d32169eb1e21f79df893e8458cf9825b03f8c5ea
Ntungufhadzeni <mbudzenin@thenakedscientists.co.za>
2023-08-22 18:15:53 +0200

    debug models

--- cover_letter/models.py
+++ cover_letter/models.py
@@ -1,23 +1,16 @@
-import uuid
-
 from django.db import models
 from django.utils import timezone
+import uuid
 
 
-class TimeStampedModel(models.Model):
-    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, max_length=34)
+class CoverLetterSubscriber(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
     created_at = models.DateTimeField(default=timezone.now)
-
-    class Meta:
-        abstract = True
-
-
-class CoverLetterSubscriber(TimeStampedModel):
     whatsapp_name = models.CharField(max_length=100)
     whatsapp_number = models.CharField(max_length=11, unique=True)
 
     class Meta:
-        ordering = ["-created_at"]
+        ordering = ("-created_at",)
 
     def __str__(self):
         return f'{self.whatsapp_name}'
