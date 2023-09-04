02a1c7b4ae24a4d4cd5afbaa9ae6e9f64580f54b
Ntungufhadzeni <mbudzenin@thenakedscientists.co.za>
2023-09-04 06:44:47 +0200

    add update results task

--- bets/tasks.py
+++ bets/tasks.py
@@ -18,25 +18,46 @@ def get_matches_api():
     response = requests.get(settings.API_URL, headers=headers, params=params)
     if response.status_code == 200:
         data = response.json()['data']
-        save_data(data)
+        if data:
+            for entry in data:
+                start_date = datetime.datetime.strptime(entry['start_date'], '%Y-%m-%dT%H:%M:%S') + datetime.timedelta(
+                    hours=1)
+                match = Match(
+                    id=entry['id'],
+                    country=entry['competition_cluster'],
+                    competition=entry['competition_name'],
+                    federation=entry['federation'],
+                    home_team=entry['home_team'],
+                    away_team=entry['away_team'],
+                    start_date=start_date,
+                    bet=entry['prediction'],
+                    result=entry['result'],
+                    status=entry['status']
+                )
+                match.save()
+            return data
+        else:
+            return response.json()
 
 
-@shared_task(name='save_data')
-def save_data(data):
-    if data:
-        for entry in data:
-            start_date = datetime.datetime.strptime(entry['start_date'], '%Y-%m-%dT%H:%M:%S') + datetime.timedelta(
-                hours=1)
-            match = Match(
-                id=entry['id'],
-                country=entry['competition_cluster'],
-                competition=entry['competition_name'],
-                federation=entry['federation'],
-                home_team=entry['home_team'],
-                away_team=entry['away_team'],
-                start_date=start_date,
-                bet=entry['prediction'],
-                result=entry['result'],
-                status=entry['status']
-            )
-            match.save()
+@shared_task(name='update_matches_results_from_api')
+def update_matches_results():
+    date = datetime.datetime.now().strftime('%Y-%m-%d')
+    params = {"market": "classic", "iso_date": date, "federation": "UEFA"}
+    headers = {
+        "X-RapidAPI-Key": settings.X_RAPIDAPI_KEY,
+        "X-RapidAPI-Host": settings.X_RAPIDAPI_HOST
+    }
+    response = requests.get(settings.API_URL, headers=headers, params=params)
+    if response.status_code == 200:
+        data = response.json()['data']
+        if data:
+            for entry in data:
+                match = Match.objects.filter(id=entry['id']).first()
+                if match:
+                    match.result = entry['result']
+                    match.status = entry['status']
+                    match.save()
+            return data
+        else:
+            response.json()
